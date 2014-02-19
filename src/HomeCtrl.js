745ec5ac5a6c4943a1a5d7eb1e70902edf252913
gudmundurste12 <gudmundurste12@ru.is>
2014-02-19 19:20:39 +0000

    Cleaned some code

--- src/HomeCtrl.js
+++ src/HomeCtrl.js
@@ -1,23 +1,19 @@
 angular.module("ChatApp").controller("HomeCtrl", ["$scope", "$location", function($scope, $location){
 	var socket = io.connect('http://localhost:8080');
 	$scope.userName = "";
+	
 	$scope.connect = function() {
 		socket.emit("adduser", $scope.userName, function(available){
 			if(available){
 				//The user name is not taken
 				console.log("available");
-				succeeded = true;
 			}
 			else{
 				//The user name is taken
 				console.log("not available");
-				succeeded = false;
 			}
 		});
 	};
-	
-	
-	
 	
 	
 	
