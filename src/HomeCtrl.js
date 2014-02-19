6f75d20b61fac43d2d0fd972542698cebe0d901d
gudmundurste12 <gudmundurste12@ru.is>
2014-02-19 16:25:56 +0000

    Added some error handling to user name selection

--- src/HomeCtrl.js
+++ src/HomeCtrl.js
@@ -1,8 +1,16 @@
 angular.module("ChatApp").controller("HomeCtrl", ["$scope", function($scope){
 	var userName = prompt("Please select a user name");
-	while(false) //Replace with some condition
+	
+	while((userName == null) || false) //Replace false with some condition, check if the userName exists
 	{
-		userName = prompt("That user name is already taken\nPlease select a different user name");
+		if(userName == null)
+		{
+			userName = prompt("You must select a user name");
+		}
+		else
+		{
+			userName = prompt("That user name is already taken\nPlease select a different user name");
+		}
 	}
 	$scope.userName = userName;
 }]);
