0e18987eeab68cfa4751e278bbfc98d68f3f645f
gudmundurste12 <gudmundurste12@ru.is>
2014-02-19 16:04:20 +0000

    Added some routing information

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,10 @@
 //Set up the module with dependencies
 angular.module("ChatApp", ['ngRoute']);
 
-angular.module("ChatApp").controller("MainCtrl", ["$scope", function($scope){
-	var userName = prompt("Please select a user name");
-	while(false) //Replace with some condition
-	{
-		userName = prompt("That user name is already taken\nPlease select a different user name");
-	}
-	$scope.userName = userName;
-}]);
 
-angular.module("ChatApp").config(function($routeProvider){
-	
-});
+angular.module("ChatApp").config(["$routeProvider", function($routeProvider){
+	$routeProvider.when("/", {
+		templateUrl: "views/Home.html",
+		controller: "HomeCtrl"
+	});
+}]);
