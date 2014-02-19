489de910689b9539b00ea13fed7202767b9e4fd2
gudmundurste12 <gudmundurste12@ru.is>
2014-02-19 19:55:51 +0000

    Added some error messages to user name selection, but it always shows up one click too late

--- src/HomeCtrl.js
+++ src/HomeCtrl.js
@@ -1,16 +1,32 @@
 angular.module("ChatApp").controller("HomeCtrl", ["$scope", "$location", function($scope, $location){
 	var socket = io.connect('http://localhost:8080');
 	$scope.userName = "";
+	$scope.errorMessage;
 	
 	$scope.connect = function() {
 		socket.emit("adduser", $scope.userName, function(available){
 			if(available){
-				//The user name is not taken
-				console.log("available");
+				if($scope.userName === ""){
+					$scope.errorMessage = "Please select a user name";
+				}
+				else
+				{
+					//The user name is not taken
+					console.log("available");
+					$scope.errorMessage = "";
+					//TODO: Add the user
+				}
 			}
 			else{
-				//The user name is taken
+				//The user name is taken or something else is wrong
 				console.log("not available");
+				console.log($scope.userName);
+				if($scope.userName === ""){
+					$scope.errorMessage = "Please select a user name";
+				}
+				else{
+					$scope.errorMessage = "That user name is already taken";
+				}
 			}
 		});
 	};
