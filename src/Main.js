25195f8e85498bd077d6dfddaa4bb72c2be6fd45
gudmundurste12 <gudmundurste12@ru.is>
2014-02-24 14:08:58 +0000

    Added a new view along with routing and such

--- src/Main.js
+++ src/Main.js
@@ -6,8 +6,11 @@ angular.module("ChatApp").config(["$routeProvider", function($routeProvider){
 	$routeProvider.when("/", {
 		templateUrl: "views/Login.html",
 		controller: "LoginCtrl"
-	}).when("/Home/:roomName", {
+	}).when("/Home/Lobby", {
 		templateUrl: "views/Home.html",
+		controller: "HomeCtrl"
+	}).when("/Home/:roomName", {
+		templateUrl: "views/ChatRoom.html",
 		controller: "HomeCtrl"
 	}).otherwise({redirectTo: "/"});
 }]);
