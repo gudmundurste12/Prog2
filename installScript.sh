7ad320edf9ebcaf8cb682f50a80e19a17d7916ad
gudmundurste12 <gudmundurste12@ru.is>
2014-02-25 20:07:11 +0000

    Made the installscript.sh shorter while not removing any functionality

--- installScript.sh
+++ installScript.sh
@@ -1,8 +1,2 @@
 	bower install angular angular-route jquery bootstrap
-
-	npm install grunt-contrib-clean
-	npm install grunt-contrib-jshint
-	npm install grunt-contrib-concat
-	npm install grunt-contrib-connect
-	npm install grunt-contrib-watch
-	npm install grunt-contrib-uglify
+	npm install
