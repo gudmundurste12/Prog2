e8b1cf3c66a6a26b1e060482a98be38d136f0c15
viktorsmari <viktorsmari@gmail.com>
2014-02-25 17:18:53 +0000

    Installscript added

---
+++ installScript.sh
@@ -0,0 +1,8 @@
+	bower install angular angular-route jquery bootstrap
+
+	npm install grunt-contrib-clean
+	npm install grunt-contrib-jshint
+	npm install grunt-contrib-concat
+	npm install grunt-contrib-connect
+	npm install grunt-contrib-watch
+	npm install grunt-contrib-uglify
